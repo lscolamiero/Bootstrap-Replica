332b9fabda0a34875976a8a822c8ed16dd7df38b
lscolamiero <lautaro.scolamiero@gmail.com>
2024-01-20 02:35:03 -0800

    react bootstrap replica from instacart created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import TopBar from './components/TopBar';
+import Background from './components/Background';
+import StoreListings from './components/StoreListing';
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Background />
+      <StoreListings />
     </div>
   );
 }
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,25 +2,23 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from "react-bootstrap/Nav"
 import Button from "react-bootstrap/Button"
 import Form from 'react-bootstrap/Form';
+import '../App.css';
 
 export default function TopBar() {
     return (
-        <Navbar bg="light" expand="lg" >
-            <Navbar.Brand style={{ margin: "5px 0 0 20px" }} href="#home">
+        <div className='sticky'>
+            <Navbar bg="light" expand="lg" style={{ width: "130%" }}>
                 <img src="https://www.instacart.com/assets/beetstrap/brand/2022/instacart-logo-color-6678cb82d531f8910d5ba270a11a7e9b56fc261371bda42ea7a5abeff3492e1c.svg" alt="instacart logo" />
-            </Navbar.Brand>
-            <Form>
-                <Form.Control size="lg" style={{ width: "1350px", margin: "20px" }} type="search-bar" placeholder="Search products and stores" />
-            </Form>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
+                <Form>
+                    <Form.Control size="lg" style={{ width: "50vw", margin: "20px" }} type="search-bar" placeholder="Search products and stores" />
+                </Form>
                 <Nav className="mr-auto">
                 </Nav>
                 <Nav>
-                    <Nav.Link href="#LogIn">Log In</Nav.Link>
-                    <Button variant="success">Sign Up</Button>{' '}
+                    <Nav.Link href="#LogIn"><b>Log In</b></Nav.Link>
+                    <Button style={{ width: "100px", fontWeight: "bolder", borderRadius: "55px" }} variant="success">Sign Up</Button>{' '}
                 </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+            </Navbar>
+        </div>
     )
 }
